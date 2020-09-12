ed72ca37434011e590a5a24d55390f82dc53594f
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2020-09-12 20:21:34 +0200

    Cargo fmt and EUPL

--- simplex/src/lib.rs
+++ simplex/src/lib.rs
@@ -4,33 +4,32 @@ use ndarray::*;
 mod test;
 
 /// Constraints you can add to your LP problem
-/// 
+///
 /// Equal is x + y + z = N
 /// LessThan is x + y + z <= N
 /// GreaterThan is x + y + z >= N
-pub enum SimplexConstraint{
+pub enum SimplexConstraint {
     Equal(Vec<f64>, f64),
     LessThan(Vec<f64>, f64),
     GreaterThan(Vec<f64>, f64),
 }
 
 impl SimplexConstraint {
-    fn get_vector(&self) -> &Vec<f64>{
+    fn get_vector(&self) -> &Vec<f64> {
         match self {
-            SimplexConstraint::Equal(a,b) => a,
-            SimplexConstraint::LessThan(a,b) => a,
-            SimplexConstraint::GreaterThan(a,b) => a,
+            SimplexConstraint::Equal(a, _b) => a,
+            SimplexConstraint::LessThan(a, _b) => a,
+            SimplexConstraint::GreaterThan(a, _b) => a,
         }
     }
 
     fn get_b(&self) -> f64 {
         match self {
-            SimplexConstraint::Equal(a,b) => *b,
-            SimplexConstraint::LessThan(a,b) => *b,
-            SimplexConstraint::GreaterThan(a,b) => *b,
+            SimplexConstraint::Equal(_a, b) => *b,
+            SimplexConstraint::LessThan(_a, b) => *b,
+            SimplexConstraint::GreaterThan(_a, b) => *b,
         }
     }
-
 }
 
 #[derive(Clone, Debug, PartialEq)]
@@ -42,22 +41,22 @@ pub enum SimplexVar {
 }
 
 impl SimplexVar {
-    fn is_artificial(&self) -> bool{
+    fn is_artificial(&self) -> bool {
         match self {
             SimplexVar::Artificial(_) => true,
-            _ => false
+            _ => false,
         }
     }
 
-    fn is_slack(&self) -> bool{
+    fn is_slack(&self) -> bool {
         match self {
             SimplexVar::Slack(_) => true,
-            _ => false
+            _ => false,
         }
     }
 }
 /// The result of a Simplex calculation
-/// 
+///
 /// UniqueOptimum means there's only one solution, and is an optimum
 /// MultipleOptimum means there's an optimum, but with different solutions. Run solve again to get another solution.
 /// InfiniteSolution means the problem is unbound, so the optimum is infinite
@@ -70,7 +69,7 @@ pub enum SimplexOutput {
     NoSolution,
 }
 
-pub struct SimplexTable{
+pub struct SimplexTable {
     objective: Vec<f64>,
     table: Array2<f64>,
     base: Vec<usize>,
@@ -78,11 +77,11 @@ pub struct SimplexTable{
 }
 
 impl SimplexTable {
-    fn get_entry_var(&self) -> Option<usize>{
+    fn get_entry_var(&self) -> Option<usize> {
         let mut entry_var = None;
         let mut max_entry = -1.0;
-        for (i, z) in self.table.row(0).iter().enumerate(){
-            if i == 0 || i == self.table.ncols()-1{
+        for (i, z) in self.table.row(0).iter().enumerate() {
+            if i == 0 || i == self.table.ncols() - 1 {
                 continue;
             }
             if max_entry < *z {
@@ -96,30 +95,30 @@ impl SimplexTable {
     fn get_exit_var(&self, entry_var: usize) -> Option<usize> {
         let mut exit_var = None;
         let mut min_entry = f64::MAX;
-        let b = self.table.column(self.table.ncols()-1);
-        for (i, z) in self.table.column(entry_var).iter().enumerate(){
+        let b = self.table.column(self.table.ncols() - 1);
+        for (i, z) in self.table.column(entry_var).iter().enumerate() {
             if i == 0 {
                 continue;
             }
             if *z <= 0.0 {
                 continue;
             }
-            if min_entry > b[i]/z{
-                min_entry = b[i]/z;
-                exit_var = Some(self.base[i-1]);
+            if min_entry > b[i] / z {
+                min_entry = b[i] / z;
+                exit_var = Some(self.base[i - 1]);
             }
         }
         exit_var
     }
 
-    fn step(&mut self, entry_var: usize, exit_var: usize){
-        let exit_row = self.base.iter().position(|x| *x == exit_var).unwrap() +1;
+    fn step(&mut self, entry_var: usize, exit_var: usize) {
+        let exit_row = self.base.iter().position(|x| *x == exit_var).unwrap() + 1;
         let pivot = self.table.row(exit_row)[entry_var];
         {
             let mut row = self.table.row_mut(exit_row);
             row /= pivot;
         }
-        for i in 0..self.table.nrows(){
+        for i in 0..self.table.nrows() {
             if i == exit_row {
                 continue;
             }
@@ -129,17 +128,15 @@ impl SimplexTable {
             exit_row *= factor;
             row += &exit_row;
         }
-        self.base = self.base.iter_mut()
-            .map(|x| if *x == exit_var {
-                entry_var
-            }else{
-                *x
-            })
+        self.base = self
+            .base
+            .iter_mut()
+            .map(|x| if *x == exit_var { entry_var } else { *x })
             .collect();
     }
 
     /// Solve your LP problem
-    /// 
+    ///
     /// Try to solve the LP problem. It uses the "standard" Simplex algorithm, with Big M method
     /// There's no timeout, so it could run for a very long time if you're not careful.
     /// It returns a SimplexOutput, which has a description of the solution and the optimum value (if exists).
@@ -156,33 +153,33 @@ impl SimplexTable {
     ///    assert_eq!(simplex.get_var(2), Some(1.5));
     ///    assert_eq!(simplex.get_var(3), Some(1.0));
     /// ```
-    pub fn solve(&mut self) -> SimplexOutput{
+    pub fn solve(&mut self) -> SimplexOutput {
         loop {
-            if let Some(entry_var) = self.get_entry_var(){
-                if let Some(exit_var) = self.get_exit_var(entry_var){
+            if let Some(entry_var) = self.get_entry_var() {
+                if let Some(exit_var) = self.get_exit_var(entry_var) {
                     self.step(entry_var, exit_var);
-                }else{
+                } else {
                     return SimplexOutput::InfiniteSolution;
                 }
-            }else{
+            } else {
                 panic!("Can't continue");
             }
             let mut optimum = true;
             let mut unique = true;
             for (i, &z) in self.table.row(0).iter().skip(1).enumerate() {
                 optimum = optimum && z <= 0.0;
-                if !self.base.contains(&i) && i <= self.objective.len(){
-                    unique =  unique && z - self.objective[i] < 0.0;
+                if !self.base.contains(&i) && i <= self.objective.len() {
+                    unique = unique && z - self.objective[i] < 0.0;
                 }
             }
             if optimum {
-                let optimum = self.table.row(0)[self.table.ncols()-1];
+                let optimum = self.table.row(0)[self.table.ncols() - 1];
                 for (i, var) in self.base.iter().enumerate() {
-                    if self.vars[*var-1].is_artificial(){
-                        if self.table.row(i+1)[self.table.ncols()-1] > 0.0{
+                    if self.vars[*var - 1].is_artificial() {
+                        if self.table.row(i + 1)[self.table.ncols() - 1] > 0.0 {
                             /* Artificial variable might have taken slack var value */
-                            if self.vars[*var-2].is_slack(){
-                                if self.table.row(0)[*var-1] == 0.0 {
+                            if self.vars[*var - 2].is_slack() {
+                                if self.table.row(0)[*var - 1] == 0.0 {
                                     continue;
                                 }
                             }
@@ -195,13 +192,12 @@ impl SimplexTable {
                 } else {
                     return SimplexOutput::MultipleOptimum(optimum);
                 }
-
             }
         }
     }
 
     /// Gets the value of the N var in a solved problem
-    /// 
+    ///
     /// ```rust
     ///    let program = Simplex::minimize(&vec![-3.0, 1.0, -2.0])
     ///    .with(vec![
@@ -215,29 +211,29 @@ impl SimplexTable {
     ///    assert_eq!(simplex.get_var(2), Some(1.5));
     ///    assert_eq!(simplex.get_var(3), Some(1.0));
     /// ```
-    pub fn get_var(&self, var: usize) -> Option<f64>{
+    pub fn get_var(&self, var: usize) -> Option<f64> {
         if var > self.objective.len() {
             return None;
         }
         for (i, v) in self.base.iter().enumerate() {
             if *v == var {
-                return Some(self.table.row(i+1)[self.table.ncols()-1]);
+                return Some(self.table.row(i + 1)[self.table.ncols() - 1]);
             }
         }
         return Some(0.0);
     }
 }
 
-pub struct SimplexMinimizerBuilder{
+pub struct SimplexMinimizerBuilder {
     objective: Vec<f64>,
 }
 
 impl SimplexMinimizerBuilder {
     /// Add constraints to the problem
-    /// 
+    ///
     /// Add constraints to your problem. All variables are already restricted to be equal or more than zero.
     /// Constraints must be of type SimplexConstraint. It will return a Result. If the generated matrix is not valid (wrong dimensions,...), it will return an Err(String).
-    /// 
+    ///
     /// ```rust
     /// let mut simplex = Simplex::minimize(&vec![1.0, -2.0])
     /// .with(vec![
@@ -254,20 +250,20 @@ impl SimplexMinimizerBuilder {
     ///      -x +y >= 1
     ///      y <= 3
     /// ```
-    pub fn with(self, constraints: Vec<SimplexConstraint>) -> Result<SimplexTable, String>{
+    pub fn with(self, constraints: Vec<SimplexConstraint>) -> Result<SimplexTable, String> {
         let mut table = Vec::new();
         let mut vars = Vec::new();
         table.push(1.0);
-        for var in self.objective.iter(){
+        for var in self.objective.iter() {
             table.push(var * -1.0);
             vars.push(SimplexVar::Real);
         }
-        for (i, constraint) in constraints.iter().enumerate(){
+        for (i, constraint) in constraints.iter().enumerate() {
             match constraint {
                 SimplexConstraint::LessThan(_, _) => {
                     table.push(0.0);
                     vars.push(SimplexVar::Slack(i));
-                },
+                }
                 SimplexConstraint::GreaterThan(_, _) => {
                     table.push(0.0);
                     vars.push(SimplexVar::NegativeSlack(i));
@@ -279,62 +275,65 @@ impl SimplexMinimizerBuilder {
         }
         table.push(0.0);
 
-        for (i, constraint) in constraints.iter().enumerate(){
+        for (i, constraint) in constraints.iter().enumerate() {
             table.push(0.0);
-            for a in constraint.get_vector(){
+            for a in constraint.get_vector() {
                 table.push(*a);
             }
-            for var in vars.iter(){
-                match var{
-                    SimplexVar::Slack(j) => if *j == i {
-                        table.push(1.0);
-                    }else{
-                        table.push(0.0);
-                    },
-                    SimplexVar::NegativeSlack(j) => if *j == i {
-                        table.push(-1.0);
-                    }else{
-                        table.push(0.0);
-                    },
-                    SimplexVar::Artificial(j) => if *j == i {
-                        table.push(1.0);
-                    }else{
-                        table.push(0.0);
+            for var in vars.iter() {
+                match var {
+                    SimplexVar::Slack(j) => {
+                        if *j == i {
+                            table.push(1.0);
+                        } else {
+                            table.push(0.0);
+                        }
+                    }
+                    SimplexVar::NegativeSlack(j) => {
+                        if *j == i {
+                            table.push(-1.0);
+                        } else {
+                            table.push(0.0);
+                        }
+                    }
+                    SimplexVar::Artificial(j) => {
+                        if *j == i {
+                            table.push(1.0);
+                        } else {
+                            table.push(0.0);
+                        }
                     }
                     _ => {}
                 }
             }
             table.push(constraint.get_b());
         }
-        let base: Vec<usize> = vars.iter()
+        let base: Vec<usize> = vars
+            .iter()
             .enumerate()
-            .filter_map(|(i, x)| if x.is_artificial() {
-                Some(i+1)
-            }else{
-                None
-            })
+            .filter_map(|(i, x)| if x.is_artificial() { Some(i + 1) } else { None })
             .collect();
-        let table = Array2::from_shape_vec((base.len() + 1, vars.len()+2), table);
+        let table = Array2::from_shape_vec((base.len() + 1, vars.len() + 2), table);
         match table {
-            Ok(table) => Ok(SimplexTable{
+            Ok(table) => Ok(SimplexTable {
                 objective: self.objective,
                 table: table,
                 base: base,
                 vars: vars,
             }),
-            Err(_) => Err(String::from("Invalid matrix"))
+            Err(_) => Err(String::from("Invalid matrix")),
         }
     }
 }
 
 /// Initialize your Linnear Programming problem
-/// 
+///
 /// Use it at the beginning, to define your problem.
-/// 
+///
 /// ```rust
 /// let mut problem = Simplex::minimize(&vec![5.0, -6.0]);
 /// ```
-/// 
+///
 pub struct Simplex;
 
 impl Simplex {
@@ -346,9 +345,9 @@ impl Simplex {
     // let mut problem = Simplex::minimize(&vec![5.0, -6.0]);
     // ```
     // It initializes Simplex with a minimization objective function z = 5x - 6y
-    pub fn minimize(objective: &Vec<f64>) -> SimplexMinimizerBuilder{
-        SimplexMinimizerBuilder{
-            objective: objective.clone()
+    pub fn minimize(objective: &Vec<f64>) -> SimplexMinimizerBuilder {
+        SimplexMinimizerBuilder {
+            objective: objective.clone(),
         }
     }
 }
--- simplex/src/test.rs
+++ simplex/src/test.rs
@@ -2,65 +2,97 @@ use crate::*;
 use ndarray::arr2;
 
 #[test]
-fn build_simplex_table_1(){
-    let program = Simplex::minimize(&vec![-3.0, 1.0, -2.0])
-        .with(vec![
-            SimplexConstraint::LessThan(vec![2.0, -2.0, 3.0], 5.0),
-            SimplexConstraint::LessThan(vec![1.0, 1.0, -1.0], 3.0),
-            SimplexConstraint::LessThan(vec![1.0, -1.0, 1.0], 2.0),
-        ]);
+fn build_simplex_table_1() {
+    let program = Simplex::minimize(&vec![-3.0, 1.0, -2.0]).with(vec![
+        SimplexConstraint::LessThan(vec![2.0, -2.0, 3.0], 5.0),
+        SimplexConstraint::LessThan(vec![1.0, 1.0, -1.0], 3.0),
+        SimplexConstraint::LessThan(vec![1.0, -1.0, 1.0], 2.0),
+    ]);
     let simplex = program.unwrap();
-    assert_eq!(simplex.table, arr2(&[
-        [1.0, 3.0, -1.0, 2.0, 0.0, f64::MIN, 0.0, f64::MIN, 0.0, f64::MIN, 0.0],
-        [0.0, 2.0, -2.0, 3.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 5.0],
-        [0.0, 1.0, 1.0, -1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 3.0],
-        [0.0, 1.0, -1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 2.0]
-    ]));
+    assert_eq!(
+        simplex.table,
+        arr2(&[
+            [
+                1.0,
+                3.0,
+                -1.0,
+                2.0,
+                0.0,
+                f64::MIN,
+                0.0,
+                f64::MIN,
+                0.0,
+                f64::MIN,
+                0.0
+            ],
+            [0.0, 2.0, -2.0, 3.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 5.0],
+            [0.0, 1.0, 1.0, -1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 3.0],
+            [0.0, 1.0, -1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 2.0]
+        ])
+    );
 
     assert_eq!(simplex.base, vec![5, 7, 9]);
     assert_eq!(simplex.vars.len(), 9);
 }
 
 #[test]
-fn build_simplex_table_2(){
+fn build_simplex_table_2() {
     let simplex = Simplex::minimize(&vec![-1.0, 1.0])
         .with(vec![
             SimplexConstraint::LessThan(vec![3.0, -4.0], 12.0),
-            SimplexConstraint::LessThan(vec![1.0, -1.0], 10.0)
-        ]).unwrap();
+            SimplexConstraint::LessThan(vec![1.0, -1.0], 10.0),
+        ])
+        .unwrap();
 
-    assert_eq!(simplex.table, arr2(&[
-        [1.0, 1.0, -1.0, 0.0, f64::MIN, 0.0, f64::MIN, 0.0],
-        [0.0, 3.0, -4.0, 1.0, 1.0, 0.0, 0.0, 12.0],
-        [0.0, 1.0, -1.0, 0.0, 0.0, 1.0, 1.0, 10.0]
-    ]));
+    assert_eq!(
+        simplex.table,
+        arr2(&[
+            [1.0, 1.0, -1.0, 0.0, f64::MIN, 0.0, f64::MIN, 0.0],
+            [0.0, 3.0, -4.0, 1.0, 1.0, 0.0, 0.0, 12.0],
+            [0.0, 1.0, -1.0, 0.0, 0.0, 1.0, 1.0, 10.0]
+        ])
+    );
 
     assert_eq!(simplex.base, vec![4, 6]);
     assert_eq!(simplex.vars.len(), 6);
 }
 
 #[test]
-fn build_simplex_table_3(){
+fn build_simplex_table_3() {
     let simplex = Simplex::minimize(&vec![-1.0, 2.0])
         .with(vec![
             SimplexConstraint::GreaterThan(vec![1.0, 1.0], 2.0),
             SimplexConstraint::GreaterThan(vec![-1.0, 1.0], 1.0),
-            SimplexConstraint::LessThan(vec![0.0, 1.0], 3.0)
-        ]).unwrap();
-    assert_eq!(simplex.table, arr2(&[
-        [1.0, 1.0, -2.0, 0.0, f64::MIN, 0.0, f64::MIN, 0.0, f64::MIN, 0.0],
-        [0.0, 1.0, 1.0, -1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 2.0],
-        [0.0, -1.0, 1.0, 0.0, 0.0, -1.0, 1.0, 0.0, 0.0, 1.0],
-        [0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 3.0]
-    ]));
+            SimplexConstraint::LessThan(vec![0.0, 1.0], 3.0),
+        ])
+        .unwrap();
+    assert_eq!(
+        simplex.table,
+        arr2(&[
+            [
+                1.0,
+                1.0,
+                -2.0,
+                0.0,
+                f64::MIN,
+                0.0,
+                f64::MIN,
+                0.0,
+                f64::MIN,
+                0.0
+            ],
+            [0.0, 1.0, 1.0, -1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 2.0],
+            [0.0, -1.0, 1.0, 0.0, 0.0, -1.0, 1.0, 0.0, 0.0, 1.0],
+            [0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 3.0]
+        ])
+    );
     assert_eq!(simplex.base, vec![4, 6, 8]);
     assert_eq!(simplex.vars.len(), 8);
 }
 
 #[test]
-fn get_entry_var_1(){
-    let program = Simplex::minimize(&vec![-3.0, 1.0, -2.0])
-    .with(vec![
+fn get_entry_var_1() {
+    let program = Simplex::minimize(&vec![-3.0, 1.0, -2.0]).with(vec![
         SimplexConstraint::LessThan(vec![2.0, -2.0, 3.0], 5.0),
         SimplexConstraint::LessThan(vec![1.0, 1.0, -1.0], 3.0),
         SimplexConstraint::LessThan(vec![1.0, -1.0, 1.0], 2.0),
@@ -70,30 +102,31 @@ fn get_entry_var_1(){
 }
 
 #[test]
-fn get_entry_var_2(){
+fn get_entry_var_2() {
     let simplex = Simplex::minimize(&vec![-1.0, 1.0])
         .with(vec![
             SimplexConstraint::LessThan(vec![3.0, -4.0], 12.0),
-            SimplexConstraint::LessThan(vec![1.0, -1.0], 10.0)
-        ]).unwrap();
+            SimplexConstraint::LessThan(vec![1.0, -1.0], 10.0),
+        ])
+        .unwrap();
     assert_eq!(simplex.get_entry_var(), Some(1));
 }
 
 #[test]
-fn get_entry_var_3(){
+fn get_entry_var_3() {
     let simplex = Simplex::minimize(&vec![-1.0, 2.0])
         .with(vec![
             SimplexConstraint::GreaterThan(vec![1.0, 1.0], 2.0),
             SimplexConstraint::GreaterThan(vec![-1.0, 1.0], 1.0),
-            SimplexConstraint::LessThan(vec![0.0, 1.0], 3.0)
-        ]).unwrap();
+            SimplexConstraint::LessThan(vec![0.0, 1.0], 3.0),
+        ])
+        .unwrap();
     assert_eq!(simplex.get_entry_var(), Some(1));
 }
 
 #[test]
-fn get_exit_var_1(){
-    let program = Simplex::minimize(&vec![-3.0, 1.0, -2.0])
-    .with(vec![
+fn get_exit_var_1() {
+    let program = Simplex::minimize(&vec![-3.0, 1.0, -2.0]).with(vec![
         SimplexConstraint::LessThan(vec![2.0, -2.0, 3.0], 5.0),
         SimplexConstraint::LessThan(vec![1.0, 1.0, -1.0], 3.0),
         SimplexConstraint::LessThan(vec![1.0, -1.0, 1.0], 2.0),
@@ -103,30 +136,31 @@ fn get_exit_var_1(){
 }
 
 #[test]
-fn get_exit_var_2(){
+fn get_exit_var_2() {
     let simplex = Simplex::minimize(&vec![-1.0, 1.0])
         .with(vec![
             SimplexConstraint::LessThan(vec![3.0, -4.0], 12.0),
-            SimplexConstraint::LessThan(vec![1.0, -1.0], 10.0)
-        ]).unwrap();
+            SimplexConstraint::LessThan(vec![1.0, -1.0], 10.0),
+        ])
+        .unwrap();
     assert_eq!(simplex.get_exit_var(1), Some(4));
 }
 
 #[test]
-fn get_exit_var_3(){
+fn get_exit_var_3() {
     let simplex = Simplex::minimize(&vec![-1.0, 2.0])
         .with(vec![
             SimplexConstraint::GreaterThan(vec![1.0, 1.0], 2.0),
             SimplexConstraint::GreaterThan(vec![-1.0, 1.0], 1.0),
-            SimplexConstraint::LessThan(vec![0.0, 1.0], 3.0)
-        ]).unwrap();
+            SimplexConstraint::LessThan(vec![0.0, 1.0], 3.0),
+        ])
+        .unwrap();
     assert_eq!(simplex.get_exit_var(1), Some(4));
 }
 
 #[test]
-fn step_example_1(){
-    let program = Simplex::minimize(&vec![-3.0, 1.0, -2.0])
-    .with(vec![
+fn step_example_1() {
+    let program = Simplex::minimize(&vec![-3.0, 1.0, -2.0]).with(vec![
         SimplexConstraint::LessThan(vec![2.0, -2.0, 3.0], 5.0),
         SimplexConstraint::LessThan(vec![1.0, 1.0, -1.0], 3.0),
         SimplexConstraint::LessThan(vec![1.0, -1.0, 1.0], 2.0),
@@ -136,15 +170,12 @@ fn step_example_1(){
     let exit_var = simplex.get_exit_var(entry_var).unwrap();
     simplex.step(entry_var, exit_var);
     assert_eq!(simplex.base, vec![5, 7, 1]);
-    assert_eq!(simplex.table.column(10), arr1(
-        &[-6.0, 1.0, 1.0, 2.0]
-    ));
+    assert_eq!(simplex.table.column(10), arr1(&[-6.0, 1.0, 1.0, 2.0]));
 }
 
 #[test]
-fn solve_example_1(){
-    let program = Simplex::minimize(&vec![-3.0, 1.0, -2.0])
-    .with(vec![
+fn solve_example_1() {
+    let program = Simplex::minimize(&vec![-3.0, 1.0, -2.0]).with(vec![
         SimplexConstraint::LessThan(vec![2.0, -2.0, 3.0], 5.0),
         SimplexConstraint::LessThan(vec![1.0, 1.0, -1.0], 3.0),
         SimplexConstraint::LessThan(vec![1.0, -1.0, 1.0], 2.0),
@@ -157,34 +188,36 @@ fn solve_example_1(){
 }
 
 #[test]
-fn solve_example_2(){
+fn solve_example_2() {
     let mut simplex = Simplex::minimize(&vec![-1.0, 1.0])
         .with(vec![
             SimplexConstraint::LessThan(vec![3.0, -4.0], 12.0),
-            SimplexConstraint::LessThan(vec![1.0, -1.0], 10.0)
-        ]).unwrap();
+            SimplexConstraint::LessThan(vec![1.0, -1.0], 10.0),
+        ])
+        .unwrap();
     assert_eq!(simplex.solve(), SimplexOutput::MultipleOptimum(-10.0));
 }
 
 #[test]
-fn solve_example_3(){
+fn solve_example_3() {
     let mut simplex = Simplex::minimize(&vec![1.0, -2.0])
         .with(vec![
             SimplexConstraint::GreaterThan(vec![1.0, 1.0], 2.0),
             SimplexConstraint::GreaterThan(vec![-1.0, 1.0], 1.0),
             SimplexConstraint::LessThan(vec![0.0, 1.0], 3.0),
-        ]).unwrap();
+        ])
+        .unwrap();
     assert_eq!(simplex.solve(), SimplexOutput::MultipleOptimum(-6.0));
 }
 
 #[test]
-fn solve_example_4(){
+fn solve_example_4() {
     let mut simplex = Simplex::minimize(&vec![-2.0, -3.0, -4.0])
-    .with(vec![
-        SimplexConstraint::LessThan(vec![3.0, 2.0, 1.0], 10.0),
-        SimplexConstraint::LessThan(vec![2.0, 5.0, 3.0], 15.0),
-    ])
-    .unwrap();
+        .with(vec![
+            SimplexConstraint::LessThan(vec![3.0, 2.0, 1.0], 10.0),
+            SimplexConstraint::LessThan(vec![2.0, 5.0, 3.0], 15.0),
+        ])
+        .unwrap();
     assert_eq!(simplex.solve(), SimplexOutput::MultipleOptimum(-20.0));
     assert_eq!(simplex.get_var(1), Some(0.0));
     assert_eq!(simplex.get_var(2), Some(0.0));
@@ -192,12 +225,10 @@ fn solve_example_4(){
 }
 
 #[test]
-fn solve_example_5(){
+fn solve_example_5() {
     let mut simplex = Simplex::minimize(&vec![-1.0])
-    .with(vec![
-        SimplexConstraint::LessThan(vec![1.0], 1.0)
-    ])
-    .unwrap();
+        .with(vec![SimplexConstraint::LessThan(vec![1.0], 1.0)])
+        .unwrap();
     assert_eq!(simplex.solve(), SimplexOutput::MultipleOptimum(-1.0));
     assert_eq!(simplex.get_var(1), Some(1.0));
 }
